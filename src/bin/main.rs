deb4682168ee8f9b0b9d426e73578481e3174b75
Dominik Łapiński <dominiklapinski.pl@gmail.com>
2022-04-16 22:34:01 +0200

    Removes App struct

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -23,90 +23,74 @@ enum Event<I> {
     Tick,
 }
 
-pub struct App {
-    main_screen: DualPaneList,
-    input_channel: Receiver<Event<KeyEvent>>,
+fn spawn_sender() -> Receiver<Event<KeyEvent>> {
+    let (tx, rx) = mpsc::channel();
+    let tick_rate = Duration::from_millis(75);
+
+    thread::spawn(move || {
+        let mut last_tick = Instant::now();
+
+        loop {
+            let timeout = tick_rate
+                .checked_sub(last_tick.elapsed())
+                .unwrap_or_else(|| Duration::from_secs(0));
+
+            if event::poll(timeout).expect("Timeout occured while polling event") {
+                if let CEvent::Key(key) = event::read().expect("Couldn't read key") {
+                    if key.code == KeyCode::Esc {
+                        tx.send(Event::Shutdown)
+                            .expect("Couldn't send shutdown event");
+                    } else {
+                        tx.send(Event::Input(key))
+                            .expect("Couldn't send user input event");
+                    }
+                }
+            }
+
+            if last_tick.elapsed() >= tick_rate {
+                if let Ok(_) = tx.send(Event::Tick) {
+                    last_tick = Instant::now();
+                }
+            }
+        }
+    });
+    rx
 }
 
-impl App {
-    fn spawn_sender() -> Receiver<Event<KeyEvent>> {
-        let (tx, rx) = mpsc::channel();
-        let tick_rate = Duration::from_millis(75);
+fn capture_terminal() -> Result<Terminal<CrosstermBackend<Stdout>>, Box<dyn Error>> {
+    enable_raw_mode()?;
+    let stdout = io::stdout();
+    let backend = CrosstermBackend::new(stdout);
+    let mut terminal = Terminal::new(backend)?;
+    terminal.clear()?;
+    Ok(terminal)
+}
 
-        thread::spawn(move || {
-            let mut last_tick = Instant::now();
+pub async fn run(bucket_name: &str) -> Result<(), Box<dyn Error>> {
+    let input_channel = spawn_sender();
+    let terminal = capture_terminal().expect("Coudn't capture terminal");
+    let s3_client: Box<dyn FileList> = Box::new(S3List::new(S3Provider::new(bucket_name).await));
+    let fs_client: Box<dyn FileList> = Box::new(FilesystemList::new());
+    let mut main_screen = DualPaneList::new(terminal, s3_client, fs_client).await;
 
-            loop {
-                let timeout = tick_rate
-                    .checked_sub(last_tick.elapsed())
-                    .unwrap_or_else(|| Duration::from_secs(0));
-
-                if event::poll(timeout).expect("Timeout occured while polling event") {
-                    if let CEvent::Key(key) = event::read().expect("Couldn't read key") {
-                        if key.code == KeyCode::Esc {
-                            tx.send(Event::Shutdown)
-                                .expect("Couldn't send shutdown event");
-                        } else {
-                            tx.send(Event::Input(key))
-                                .expect("Couldn't send user input event");
-                        }
-                    }
-                }
-
-                if last_tick.elapsed() >= tick_rate {
-                    if let Ok(_) = tx.send(Event::Tick) {
-                        last_tick = Instant::now();
-                    }
-                }
+    loop {
+        match input_channel.recv().unwrap() {
+            Event::Input(event) => main_screen.handle_event(event).await,
+            Event::Shutdown => {
+                main_screen.shutdown()?;
+                break;
             }
-        });
-        rx
-    }
-
-    fn capture_terminal() -> Result<Terminal<CrosstermBackend<Stdout>>, Box<dyn Error>> {
-        enable_raw_mode()?;
-        let stdout = io::stdout();
-        let backend = CrosstermBackend::new(stdout);
-        let mut terminal = Terminal::new(backend)?;
-        terminal.clear()?;
-        Ok(terminal)
-    }
-
-    pub async fn new(bucket_name: &str) -> App {
-        let input_channel = App::spawn_sender();
-        let terminal = App::capture_terminal().expect("Coudn't capture terminal");
-        let s3_client: Box<dyn FileList> =
-            Box::new(S3List::new(S3Provider::new(bucket_name).await));
-        let fs_client: Box<dyn FileList> = Box::new(FilesystemList::new());
-        let main_screen = DualPaneList::new(terminal, s3_client, fs_client).await;
-        App {
-            main_screen,
-            input_channel,
+            Event::Tick => main_screen
+                .render()
+                .expect("Couldn't render DualPaneList screen"),
         }
     }
-
-    pub async fn run(&mut self) -> Result<(), Box<dyn Error>> {
-        loop {
-            match self.input_channel.recv().unwrap() {
-                Event::Input(event) => self.main_screen.handle_event(event).await,
-                Event::Shutdown => {
-                    self.main_screen.shutdown()?;
-                    break;
-                }
-                Event::Tick => self
-                    .main_screen
-                    .render()
-                    .expect("Couldn't render DualPaneList screen"),
-            }
-        }
-        Ok(())
-    }
+    Ok(())
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     let args: Vec<String> = env::args().collect();
-    let mut app = App::new(&args[1]).await;
-    app.run().await?;
-    Ok(())
+    let result = run(&args[1]).await;
+    result
 }
